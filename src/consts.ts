942c8570fd732691c029133bc6eac1504c60e7dc
Eduardo Gomez <this.eduardo@gmail.com>
2026-02-27 23:28:34 -0600

    Revamp site branding, fonts, and styles.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Eduardo Gómez - Frontend Engineer & UI/UX Designer";
+export const SITE_DESCRIPTION =
+  "I have a keen eye for creating usable and engaging user interfaces that I then bring to reality through code.";
