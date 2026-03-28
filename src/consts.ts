4429b666d8dc9b4cb8f6be46494a635e76e6bdd8
Eduardo Gomez <this.eduardo@gmail.com>
2026-03-27 22:03:37 -0600

    Introduce SITE_AUTHOR and add schema.org metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Eduardo Gómez - Frontend Engineer & UI/UX Designer";
+export const SITE_AUTHOR = "Eduardo Gómez";
+export const SCHEMA_ORG = "https://schema.org";
+export const SITE_TITLE = `${SITE_AUTHOR} - Frontend Engineer & UI/UX Designer`;
 export const SITE_DESCRIPTION =
   "I have a keen eye for creating usable and engaging user interfaces that I then bring to reality through code.";
 export const BLOG_PAGE_SIZE = 12;
